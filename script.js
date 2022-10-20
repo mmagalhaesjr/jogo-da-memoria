f0a0234b5e1056855e4db8374952795505c0ef83
Marcos Magalhães Júnior <magalhaesmarcosjr@gmail.com>
2022-10-20 16:56:05 -0300

    script.js
    inserindo funções

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let quantidadeCartas 
 let txt
 
-
+const cartargifs=[
+    
+]
 
 txt='Digite a quantidade de cartas: \n Numeros entre 4 e 14, sendo eles numeros pares'
 
@@ -13,7 +15,39 @@ quantidadeCartas = Number(prompt(txt));
 while(quantidadeCartas < 4 || quantidadeCartas > 14 || quantidadeCartas % 2 !== 0){
     quantidadeCartas = Number(prompt(txt));
 }
-   
+
+
+
+function adicionarCartas() {
+
+for(let cont = 0; quantidadeCartas > cont; cont++){
+
+    var lista = document.querySelector(".container");
+
+    lista.innerHTML += `
+        <div class="card">
+            <img src="img/back.png" alt="">
+        </div>  
+     `
+    
+
+    
+
+
+
+
+        
+     
+     
+
+}
+    
+}
+
+adicionarCartas()
+
+
+
 
 
     
